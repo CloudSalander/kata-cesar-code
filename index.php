b4f4d42fa770db5fdccf737fb3ee3c5dbfa43932
Cloud <ruben.as532@gmail.com>
2023-06-24 01:25:02 +0200

    First complete solution

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+define('ASCII_ALPHABET_LENGTH',26);
+
+
+function calcAsciiCode(int $char_code): int {
+	if($char_code > 90) return $char_code - ASCII_ALPHABET_LENGTH;
+	else if($char_code < 65) return $char_code + ASCII_ALPHABET_LENGTH;
+	return $char_code;
+}
 
 function encodeCesar(string $word,int $swap): string {
 	$array_chars = str_split($word);
 	foreach($array_chars as $key => $char) {
-		//TODO: Dar la vuelta al abecedario
-		$array_chars[$key] = chr(ord($char)+$swap);  
+		var_dump(calcAsciiCode(ord($char)+$swap));
+		$array_chars[$key] = chr(calcAsciiCode(ord($char)+$swap));  
 	}
 	return implode($array_chars);
 }
